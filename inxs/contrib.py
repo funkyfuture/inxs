76f8336b40f705fd1822c4335737e6b426c05683
Frank Sachsenheim <funkyfuture@riseup.net>
2017-06-15 22:04:59 +0200

    Fixes contrib.remove_empty_elements

--- inxs/contrib.py
+++ inxs/contrib.py
@@ -9,19 +9,21 @@ from inxs.xml_utils import remove_element
 __all__ = []
 
 
-def _prepend_tail(element):
+def _append_tail_to_previous_in_stream(element):
     if not element.tail:
         return
     previous = element.getprevious()
     if previous is None:
         element.getparent().text += element.tail
+    elif previous.tail is None:
+        previous.tail = element.tail
     else:
         previous.tail += element.tail
 
 
 remove_empty_elements = Transformation(
     Rule(Not(lib.has_children, lib.has_text, lib.has_attributes, '/'),
-         (_prepend_tail, remove_element)),
+         (_append_tail_to_previous_in_stream, remove_element)),
     name='remove_empty_elements',
     traversal_order=TRAVERSE_DEPTH_FIRST | TRAVERSE_LEFT_TO_RIGHT | TRAVERSE_BOTTOM_TO_TOP
 )
