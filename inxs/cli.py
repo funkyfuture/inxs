62dcaf73e7cf213ac7e21baa43b7993f935b1774
Frank Sachsenheim <funkyfuture@riseup.net>
2017-08-19 20:04:08 +0200

    cli: log handler, rudimentary exception handling
    prefers transformation named 'main' in a module when none
    is specified.

--- inxs/cli.py
+++ inxs/cli.py
@@ -1,16 +1,27 @@
 # TODO error handling
 # TODO documentation
 # TODO tests
+# TODO allow passing arguments
 
 
 from argparse import ArgumentParser
+from traceback import print_exc
 import importlib.util
-import os
+import logging
 from pathlib import Path
+from shutil import copy2 as copy_file
+import sys
 
 from lxml import etree
 
 from inxs import Transformation
+from inxs.lib import logger
+
+
+console_log_handler = logging.StreamHandler(sys.stdout)
+console_log_handler.setLevel(logging.DEBUG)
+logger.addHandler(console_log_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def parse_args():
@@ -40,7 +51,10 @@ def get_transformation(location: str):
                               if isinstance(v, Transformation)}
 
     if transformation_name is None:
-        return next(x for x in transformation_objects.values())
+        if 'main' in transformation_objects:
+            return transformation_objects['main']
+        else:
+            return next(x for x in transformation_objects.values())
 
     for obj in transformation_objects.values():
         if obj.name == transformation_name:
@@ -51,16 +65,22 @@ def get_transformation(location: str):
 
 def apply_transformation(transformation: Transformation, target: str):
     document = etree.parse(target)
-    os.rename(target, target + '.orig')
+    copy_file(target, target + '.orig')
     root = transformation(document.getroot())
     document._setroot(root)
-    document.write(target)
+    document.write(target,
+                   encoding='utf-8',  # TODO obtain options from source
+                   xml_declaration=True)
 
 
 def main():
-    args = parse_args()
-    transformation = get_transformation(args.transformation)
-    apply_transformation(transformation, args.target)
+    try:
+        args = parse_args()
+        transformation = get_transformation(args.transformation)
+        apply_transformation(transformation, args.target)
+    except Exception as e:
+        print_exc(e)
+        raise SystemExit(2)
 
 
 if __name__ == '__main__':
