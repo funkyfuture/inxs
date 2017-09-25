f4bfb133c329c599dcd4bec8df86c11641aa0721
Frank Sachsenheim <funkyfuture@riseup.net>
2017-09-25 23:10:42 +0200

    Adds a --verbose argument to the cli.
    Also debug messages in that module.

--- inxs/cli.py
+++ inxs/cli.py
@@ -15,22 +15,27 @@ import sys
 from lxml import etree
 
 from inxs import Transformation
-from inxs.lib import logger
-
-
-console_log_handler = logging.StreamHandler(sys.stdout)
-console_log_handler.setLevel(logging.DEBUG)
-logger.addHandler(console_log_handler)
-logger.setLevel(logging.DEBUG)
+from inxs.lib import dbg, logger, nfo
 
 
 def parse_args():
     parser = ArgumentParser()
 
+    parser.add_argument('-v', '--verbose', action='count', default=0)
     parser.add_argument('transformation')
     parser.add_argument('target')
 
     return parser.parse_args()
+
+
+def setup_logging(verbosity: int):
+    level = {0: logging.WARNING,
+             1: logging.INFO,
+             2: logging.DEBUG}[verbosity],
+    console_log_handler = logging.StreamHandler(sys.stdout)
+    console_log_handler.setLevel(level)
+    logger.addHandler(console_log_handler)
+    logger.setLevel(level)
 
 
 def get_transformation(location: str):
@@ -42,40 +47,55 @@ def get_transformation(location: str):
     if not module_path.endswith('.py'):
         module_path += '.py'
     module_path = (Path.cwd() / module_path).resolve()
+    dbg(f'Transformation module path: {module_path}')
 
+    dbg('Loading module.')
     module_spec = importlib.util.spec_from_file_location('transformation_file', module_path)
     _module = importlib.util.module_from_spec(module_spec)
     module_spec.loader.exec_module(_module)
 
+    dbg('Inspecting module.')
     transformation_objects = {k: v for k, v in vars(_module).items()
                               if isinstance(v, Transformation)}
 
     if transformation_name is None:
         if 'main' in transformation_objects:
+            dbg("Selected symbol 'main' from module as transformation.")
             return transformation_objects['main']
         else:
-            return next(x for x in transformation_objects.values())
+            name, instance = next((k, v) for k, v in transformation_objects.items())
+            dbg(f"Selected symbol '{name}' from module as transformation.")
+            return instance
 
-    for obj in transformation_objects.values():
-        if obj.name == transformation_name:
-            return obj
+    for instance in transformation_objects.values():
+        if instance.name == transformation_name:
+            dbg(f"Selected transformation named '{transformation_name}' from module "
+                 "as transformation.")
+            return instance
 
+    dbg(f"Selected symbol '{transformation_name}' from module as transformation.")
     return transformation_objects[transformation_name]
 
 
 def apply_transformation(transformation: Transformation, target: str):
     document = etree.parse(target)
     copy_file(target, target + '.orig')
+    dbg("Saved document backup with suffix '.orig'")
+    dbg('Applying transformation.')
     root = transformation(document.getroot())
     document._setroot(root)
     document.write(target,
                    encoding='utf-8',  # TODO obtain options from source
                    xml_declaration=True)
+    dbg('Wrote result back to file.')
 
 
 def main():
+    nfo('Starting')
     try:
         args = parse_args()
+        setup_logging(args.verbose)
+        dbg(f'Invoked with args: {args}')
         transformation = get_transformation(args.transformation)
         apply_transformation(transformation, args.target)
     except Exception as e:
