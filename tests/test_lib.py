af6e355b447b485f77c9bad3c99af0a6dea9f0f5
Frank Sachsenheim <funkyfuture@riseup.net>
2017-10-21 22:23:48 +0200

    Adds more tests for the lib.

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,8 +1,8 @@
+from itertools import permutations
 from types import SimpleNamespace
 
 from lxml import builder, etree
 from pytest import mark
-
 
 from inxs import lib, Ref, Rule, Transformation
 from tests import equal_subtree
@@ -19,12 +19,66 @@ def test_concatenate():
     assert lib.concatenate('foo', Ref('foo'))(transformation) == 'foobar'
 
 
+@mark.parametrize('side,expected',
+                  (('left', '<r><x/><rs/></r>'),
+                   ('right', '<r><ls/><x/></r>')))
+def test_drop_siblings(side, expected):
+    doc = etree.fromstring('<r><ls/><x/><rs/></r>')
+    transformation = Transformation(
+        Rule('x', lib.drop_siblings(side))
+    )
+    result = transformation(doc)
+    assert equal_subtree(result, etree.fromstring(expected))
+
+
+def test_has_tail():
+    element = etree.Element('foo', )
+    assert not lib.has_tail(element, None)
+    element.tail = ''
+    assert not lib.has_tail(element, None)
+    element.tail = 'tail'
+    assert lib.has_tail(element, None)
+
+
+@mark.parametrize('tag,namespace_s,nsmap',
+                  (('foo', 'http://bar.org', {}),
+                   ('bar:foo', {'bar': 'http://bar.org'}, {}),
+                   ('bar:foo', None, {'bar': 'http://bar.org'})))
+def test_make_element(tag, namespace_s, nsmap):
+    handler = lib.make_element(tag, namespace_s)
+    assert etree.QName(handler(None, nsmap)).text == '{http://bar.org}foo'
+
+
 def test_merge():
     source = etree.fromstring('<root><a><bb/></a><b/></root>')
     destination = etree.fromstring('<root><a><aa/></a><b/><c/></root>')
     expected = etree.fromstring('<root><a><aa/><bb/></a><b/><c/></root>')
     transformation = Transformation(lib.merge('source'), context={'source': source})
     equal_subtree(transformation(destination), expected)
+
+
+def test_pop_attribute():
+    element = etree.Element('x', {'y': 'z'})
+    handler = lib.pop_attribute('y')
+    result = handler(element)
+    assert result == 'z'
+    assert 'y' not in element.attrib
+
+
+@mark.parametrize('keep_children,clear_ref', permutations((True, False)))
+def test_remove_elements(keep_children, clear_ref):
+    e = builder.ElementMaker()
+    child = e.b()
+    target = e.a(child)
+    root = e.root(target)
+    trash_bin = [target]
+    transformation = SimpleNamespace(_available_symbols={'trashbin': trash_bin},
+                                     states=SimpleNamespace(previous_result=None))
+    lib.remove_elements('trashbin', keep_children, clear_ref)(transformation)
+
+    assert not root.findall('a')
+    assert keep_children == bool(root.findall('b'))
+    assert clear_ref == (not bool(trash_bin))
 
 
 @mark.parametrize('ns,expected', ((None, 'rosa'), ('spartakus', '{spartakus}rosa')))
