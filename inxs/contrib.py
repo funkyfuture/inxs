585e398d2b9d05d8188823566e33c2a55da072ae
Frank Sachsenheim <funkyfuture@riseup.net>
2017-11-12 21:31:28 +0100

    contrib.remove_empty_elements can ignore tags.

--- inxs/contrib.py
+++ inxs/contrib.py
@@ -1,15 +1,20 @@
 """ This module contains transformations that are supposedly of common interest. """
 
-from inxs import \
-    (TRAVERSE_DEPTH_FIRST, TRAVERSE_BOTTOM_TO_TOP, TRAVERSE_LEFT_TO_RIGHT,
-     lib, Not, Rule, Transformation)
+from lxml import etree
+
+from inxs import (
+    TRAVERSE_DEPTH_FIRST, TRAVERSE_BOTTOM_TO_TOP, TRAVERSE_LEFT_TO_RIGHT, lib, Not, Rule,
+    SkipToNextElement, Transformation
+)
 from inxs.lxml_utils import remove_element
 
 
 __all__ = []
 
 
-def _append_tail_to_previous_in_stream(element):
+def _append_tail_to_previous_in_stream(element, skip_elements):
+    if etree.QName(element).localname in skip_elements:
+        raise SkipToNextElement
     if not element.tail:
         return
     previous = element.getprevious()
@@ -24,7 +29,7 @@ def _append_tail_to_previous_in_stream(element):
 remove_empty_elements = Transformation(
     Rule(Not(lib.has_children, lib.has_text, lib.has_attributes, '/'),
          (_append_tail_to_previous_in_stream, remove_element)),
-    name='remove_empty_elements',
+    name='remove_empty_elements', context={'skip_elements': []},
     traversal_order=TRAVERSE_DEPTH_FIRST | TRAVERSE_LEFT_TO_RIGHT | TRAVERSE_BOTTOM_TO_TOP
 )
 """ Removes elements without attributes, text, tail and children from the (sub-)tree. """
