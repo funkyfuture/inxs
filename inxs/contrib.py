9e0560713a94bf334f033cfd0c5c9cfd19efecaa
Frank Sachsenheim <funkyfuture@riseup.net>
2018-03-29 20:43:36 +0200

    contrib: Adds the reduce_whitespaces transformation
    comes handy as subtransformation

--- inxs/contrib.py
+++ inxs/contrib.py
@@ -3,12 +3,35 @@
 from lxml import etree
 
 from inxs import (
-    TRAVERSE_DEPTH_FIRST, TRAVERSE_BOTTOM_TO_TOP, TRAVERSE_LEFT_TO_RIGHT, lib, Not, Rule,
+    TRAVERSE_DEPTH_FIRST, TRAVERSE_BOTTOM_TO_TOP, TRAVERSE_LEFT_TO_RIGHT, lib, utils, Not, Rule,
     SkipToNextElement, Transformation
 )
 
 
 __all__ = []
+
+
+# reduce_whitespaces
+
+
+def _reduce_whitespace_handler(element):
+    if element.text:
+        element.text = utils.reduce_whitespaces(element.text, strip='')
+    if element.tail:
+        element.tail = utils.reduce_whitespaces(element.tail, strip='')
+
+
+reduce_whitespaces = Transformation(
+    Rule('*', _reduce_whitespace_handler)
+)
+"""
+Normalizes any whitespace character of element's text and tail to a simple space and reduces
+consecutives to one.
+"""
+__all__.append('reduce_whitespaces')
+
+
+# remove_empty_elements
 
 
 def _append_tail_to_previous_in_stream(element, skip_elements):
